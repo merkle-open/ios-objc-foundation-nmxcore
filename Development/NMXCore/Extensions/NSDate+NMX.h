09bb43ba35ab3d640bfc2280ff15cd21c2b23592
Tobias Baube <tobias.baube@namics.com>
2017-11-07 15:19:55 +0100

    NCL-4 modified functions to not throw an exception, but rather assign a nullable error pointer

--- Development/NMXCore/Extensions/NSDate+NMX.h
+++ Development/NMXCore/Extensions/NSDate+NMX.h
@@ -10,6 +10,19 @@
 
 @interface NSDate(NMX)
 
+extern NSString *_Nonnull const NMXErrorDomainNSDate;
+    
+/**
+ *  Returns the same day, but the time is altered and is 23h 59min 59s. One second before midnight (Uses NSCalendar.currentCalendar)
+ *
+ *  - Author: Adriano Segalada
+ *
+ *  - Version: 1.0
+ *
+ *  @return returns the same day, but the time is altered and will be 23:59:59. One second before midnight (Uses NSCalendar.currentCalendar). Will return "nil" on error or if unspecified
+ */
+@property (readonly, null_unspecified) NSDate *oneSecondBeforeEndOfDay;
+    
 /**
  *  Constructs a NSDate from self with a given offset in months
  *
@@ -18,10 +31,10 @@
  *  - Version: 1.0
  *
  *  @param offsetInMonths - Offset in months. Negative integers go back in time, positive numbers return dates in the future. If this parameter is nil, the same date (without offset) will be returned.
- *  @throws NSRangeException if the given offset does not allow to create a valid NSDate
+ *  @param error - if the given offset does not allow to create a valid NSDate, a NSError with ErrorDomain `NMXCore.NSDate` is assigned
  *  @return returns a NSDate instance from self with a given offset. If the offset was invalid, the same date (without offset) will be returned.
  */
-- (NSDate *)offsetInMonths:(NSInteger)offsetInMonths;
+- (NSDate *_Nullable)offsetInMonths:(NSInteger)offsetInMonths error:(NSError *_Nullable *_Nullable)error;
 
 /**
  *  Constructs a NSDate from self with a given offset in days
@@ -31,10 +44,10 @@
  *  - Version: 1.0
  *
  *  @param offsetInDays - Offset in days. Negative integers go back in time, positive numbers return dates in the future. If this parameter is nil, the same date (without offset) will be returned.
- *  @throws NSRangeException if the given offset does not allow to create a valid NSDate
+ *  @param error - if the given offset does not allow to create a valid NSDate, a NSError with ErrorDomain `NMXCore.NSDate` is assigned
  *  @return returns a NSDate instance from self with a given offset in days. If the offset was invalid, the same date (without offset) will be returned.
  */
-- (NSDate *)offsetInDays:(NSInteger)offsetInDays;
+- (NSDate *_Nullable)offsetInDays:(NSInteger)offsetInDays error:(NSError *_Nullable *_Nullable)error;
 
 /**
  *  Constructs a NSDate from self with a given offset in minutes
@@ -44,10 +57,10 @@
  *  - Version: 1.0
  *
  *  @param offsetInMinutes - Offset in minutes. Negative integers go back in time, positive numbers return dates in the future. If this parameter is nil, the same date (without offset) will be returned.
- *  @throws NSRangeException if the given offset does not allow to create a valid NSDate
+ *  @param error - if the given offset does not allow to create a valid NSDate, a NSError with ErrorDomain `NMXCore.NSDate` is assigned
  *  @return returns a NSDate instance from self with a given offset in minutes. If the offset was invalid, the same date (without offset) will be returned.
  */
-- (NSDate *)offsetInMinutes:(NSInteger)offsetInMinutes;
+- (NSDate *_Nullable)offsetInMinutes:(NSInteger)offsetInMinutes error:(NSError *_Nullable *_Nullable)error;
 
 /**
  *  Constructs a NSDate from self with a given offset in seconds
@@ -57,19 +70,9 @@
  *  - Version: 1.0
  *
  *  @param offsetInSeconds - Offset in seconds. Negative integers go back in time, positive numbers return dates in the future. If this parameter is nil, the same date (without offset) will be returned.
- *  @throws NSRangeException if the given offset does not allow to create a valid NSDate
+ *  @param error - if the given offset does not allow to create a valid NSDate, a NSError with ErrorDomain `NMXCore.NSDate` is assigned
  *  @return returns a NSDate instance from self with a given offset in seconds. If the offset was invalid, the same date (without offset) will be returned.
  */
-- (NSDate *)offsetInSeconds:(NSInteger)offsetInSeconds;
-
-/**
- *  Returns the same day, but the time is altered and is 23h 59min 59s. One second before midnight)
- *
- *  - Author: Adriano Segalada
- *
- *  - Version: 1.0
- *
- */
-@property (readonly) NSDate *oneSecondBeforeEndOfDate;
+- (NSDate *_Nullable)offsetInSeconds:(NSInteger)offsetInSeconds error:(NSError *_Nullable *_Nullable)error;
 
 @end
